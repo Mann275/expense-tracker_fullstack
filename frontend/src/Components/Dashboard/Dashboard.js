6029b7e6675c093132f5bfd4434bd65ca3c4f2a1
Mann Patel <patelmann2705@gmail.com>
2025-01-22 15:04:04 +0530

    new

--- frontend/src/Components/Dashboard/Dashboard.js
+++ frontend/src/Components/Dashboard/Dashboard.js
@@ -87,7 +87,7 @@ const DashboardStyled = styled.div`
                 }
                 .income, .expense, .balance{
                     background: #FCF6F9;
-                    border: 2px solid #FFFFFF;
+                    border: 2px solidrgb(238, 26, 26);
                     box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
                     border-radius: 20px;
                     padding: 1rem;
